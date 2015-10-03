5b569d706feec9c54e1ccee9e14294a683e88638
Adrian Todorov <adrian.todorov@outlook.com>
2015-10-03 18:00:04 -0400

    added Async task.

--- app/src/main/java/adriantodorov/estimotetounityapi/MainActivity.java
+++ app/src/main/java/adriantodorov/estimotetounityapi/MainActivity.java
@@ -1,13 +1,17 @@
 package adriantodorov.estimotetounityapi;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.EstimoteSDK;
@@ -15,6 +19,7 @@ import com.estimote.sdk.EstimoteSDK;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -23,9 +28,19 @@ public class MainActivity extends AppCompatActivity {
     TextView appId;
     TextView appToken;
 
-    EditText sendDataString;
+    EditText sendDataEditText;
+    EditText ipAddressEditText;
+
+    ProgressDialog pDialog;
+
+    String ipAddressString;
+    String dataString;
 
     Button sendData;
+
+    int port = 15000;
+
+    boolean dataSent = false;
 
     private BeaconManager beaconManager;
 
@@ -37,7 +52,8 @@ public class MainActivity extends AppCompatActivity {
         appId = (TextView) findViewById(R.id.appIdString);
         appToken = (TextView) findViewById(R.id.appTokenString);
         sendData = (Button) findViewById(R.id.sendDataButton);
-        sendDataString = (EditText) findViewById(R.id.sendDataEditText);
+        sendDataEditText = (EditText) findViewById(R.id.sendDataEditText);
+        ipAddressEditText = (EditText) findViewById(R.id.ipEditText);
 
         //  App ID & App Token can be taken from App section of Estimote Cloud.
         EstimoteSDK.initialize(getApplicationContext(), "wearhacks-2015-montreal-3mt", "e9cd76b118e99e34a719dc0ca2a1ea50");
@@ -50,56 +66,80 @@ public class MainActivity extends AppCompatActivity {
         sendData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Socket socket = null;
-                DataOutputStream dataOutputStream = null;
-                DataInputStream dataInputStream = null;
 
-                try {
-                    socket = new Socket("192.168.1.23", 15000);
-                    dataOutputStream = new DataOutputStream(socket.getOutputStream());
-                    dataInputStream = new DataInputStream(socket.getInputStream());
-                    dataOutputStream.writeUTF(sendDataString.getText().toString());
-                } catch (UnknownHostException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-                finally{
-                    if (socket != null){
-                        try {
-                            socket.close();
-                        } catch (IOException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                    }
+                ipAddressString = ipAddressEditText.getText().toString();
+                dataString = sendDataEditText.getText().toString();
 
-                    if (dataOutputStream != null){
-                        try {
-                            dataOutputStream.close();
-                        } catch (IOException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                    }
-
-                    if (dataInputStream != null){
-                        try {
-                            dataInputStream.close();
-                        } catch (IOException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                    }
-                }
+                new sendDataUnity().execute();
             }
         });
 
 
 
+
+    }
+
+    /**
+     * Background Async Task to Load all product by making HTTP Request
+     * */
+    class sendDataUnity extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Sending data to Unity...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting All trees from url
+         * */
+        protected String doInBackground(String... args) {
+
+            try
+            {
+                Log.d("sendData", "Connecting to " + ipAddressString +
+                        " on port " + port);
+                Socket client = new Socket(ipAddressString, port);
+                Log.d("sendData", "Just connected to "
+                        + client.getRemoteSocketAddress());
+                OutputStream outToServer = client.getOutputStream();
+                DataOutputStream out = new DataOutputStream(outToServer);
+                out.write(Byte.parseByte(dataString));
+                client.close();
+            }catch(IOException e)
+            {
+                Log.d("sendData", e.getStackTrace().toString());
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all trees
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+
+                    if (dataSent)
+                    {
+                        Toast.makeText(getApplicationContext(), "Data sent.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+        }
 
     }
 
