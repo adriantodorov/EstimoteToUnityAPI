b145e27c01a7d1d244e9ce35ed77f62f77b83c23
Adrian Todorov <adrian.todorov@outlook.com>
2015-10-03 14:36:13 -0400

    Added sockets.

--- app/src/main/java/adriantodorov/estimotetounityapi/MainActivity.java
+++ app/src/main/java/adriantodorov/estimotetounityapi/MainActivity.java
@@ -4,14 +4,106 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.EstimoteSDK;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView appId;
+    TextView appToken;
+
+    EditText sendDataString;
+
+    Button sendData;
+
+    private BeaconManager beaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        appId = (TextView) findViewById(R.id.appIdString);
+        appToken = (TextView) findViewById(R.id.appTokenString);
+        sendData = (Button) findViewById(R.id.sendDataButton);
+        sendDataString = (EditText) findViewById(R.id.sendDataEditText);
+
+        //  App ID & App Token can be taken from App section of Estimote Cloud.
+        EstimoteSDK.initialize(getApplicationContext(), "wearhacks-2015-montreal-3mt", "e9cd76b118e99e34a719dc0ca2a1ea50");
+        // Optional, debug logging.
+        EstimoteSDK.enableDebugLogging(true);
+
+        appId.setText(EstimoteSDK.getAppId());
+        appToken.setText(EstimoteSDK.getAppToken());
+
+        sendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Socket socket = null;
+                DataOutputStream dataOutputStream = null;
+                DataInputStream dataInputStream = null;
+
+                try {
+                    socket = new Socket("192.168.1.23", 15000);
+                    dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                    dataInputStream = new DataInputStream(socket.getInputStream());
+                    dataOutputStream.writeUTF(sendDataString.getText().toString());
+                } catch (UnknownHostException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                finally{
+                    if (socket != null){
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+
+                    if (dataOutputStream != null){
+                        try {
+                            dataOutputStream.close();
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+
+                    if (dataInputStream != null){
+                        try {
+                            dataInputStream.close();
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+
+
+
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/adriantodorov/estimotetounityapi/MyApplication.java
@@ -0,0 +1,72 @@
+package adriantodorov.estimotetounityapi;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.List;
+import java.util.UUID;
+
+public class MyApplication extends Application {
+
+    private BeaconManager beaconManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        beaconManager = new BeaconManager(getApplicationContext());
+
+        /*
+        beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
+            @Override
+            public void onEnteredRegion(Region region, List<Beacon> list) {
+                showNotification(
+                        "Beacon #1",
+                        "Beacon approached.");
+            }
+            @Override
+            public void onExitedRegion(Region region) {
+                // could add an "exit" notification too
+            }
+        });
+        */
+
+        // add this below:
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            // beacon #1 (F8:63:81:24:61:34)
+            @Override
+            public void onServiceReady() {
+                beaconManager.startMonitoring(new Region(
+                        "monitored region",
+                        UUID.fromString("b9407f30-f5f8-466e-aff9-25556b57fe6d"),
+                        24884, 33060));
+            }
+        });
+
+    } // end onCreate
+
+    public void showNotification(String title, String message) {
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
+                new Intent[] { notifyIntent }, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+    }
+} // end Application
